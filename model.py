0985e209dad1fb6b019735d6e594b5a0ef05ea86
mingyang9642 <56164240+mingyang9642@users.noreply.github.com>
2020-01-23 22:18:56 +0000

    variable change

--- model.py
+++ model.py
@@ -7,17 +7,17 @@ import agentframework as afm
 import matplotlib.pyplot as plt
 
 num_of_agents = 25
-drunks = []
-house_labels = []
+drunks_num = []
+house_num = []
 
 #Add the density map
-density = []
+density_map = []
 
 for i in range(300):
-    rowlist=[]
+    blank_list=[]
     for j in range(300):
-        rowlist.append(0)
-    density.append(rowlist)
+        blank_list.append(0)
+    density_map.append(blank_list)
         
 
 #Introduce environment raster
@@ -36,24 +36,24 @@ f.close()
 for a in range(300):
     for b in range(300):
         if environment[a][b]==1:
-           environment[a][b]=99
+           environment[a][b]=98
 
 #Determining the starting point of the drunks / labeling the house
-for j in range(num_of_agents): 
-    houseno = (j + 1) * 10
-    house_labels.append(houseno)
+for i in range(num_of_agents): 
+    houseno = (i + 1) * 10
+    house_num.append(houseno)
     
 
-for i in range(num_of_agents):
-    drunks.append(afm.Agent(environment))
+for j in range(num_of_agents):
+    drunks_num.append(afm.Agent(environment))
     
     
 #Keep moving drunks (25 starting points) until they reach the house area
-for k in range(num_of_agents):
-    while environment[drunks[k].x][drunks[k].y] != house_labels[k]:
-        drunks[k].move()
-        drunks[k].eat(density)
-    print("I am the No.", k+1, "arriving", "at", "House", int(house_labels[k]/10) )
+for n in range(num_of_agents):
+    while environment[drunks_num[n].x][drunks_num[n].y] != house_num[n]:
+        drunks_num[n].move()
+        drunks_num[n].eat(density_map)
+    print("I am the No.", n+1, "arriving", "at", "House", int(house_num[n]/10) )
 
 #Generate house-arriving map and density map
 fig=plt.figure()
@@ -65,8 +65,8 @@ patches = [ plt.plot([], marker="o", ms=10, ls="", mec=None, color="r",
             label="Drunks (solid circles filled with various colours)" )[0] ]
 plt.legend(handles=patches, loc='lower left', ncol=2, facecolor="plum", numpoints=1
            ,bbox_to_anchor=[-0.19, -0.19]) #Make the legend
-for m in range(num_of_agents):
-    plt.scatter(drunks[m].y, drunks[m].x)    
+for k in range(num_of_agents):
+    plt.scatter(drunks_num[k].y, drunks_num[k].x)    
 plt.imshow(environment, cmap = plt.cm.hot, vmin=0.1, vmax=2.5) 
 plt.show() 
 
@@ -79,7 +79,7 @@ patches = [ plt.plot([], ms=10, ls="", mec=None, color="w",
             label="Brighter the colour is, higher the density is." )[0] ]
 plt.legend(handles=patches, loc='lower left', ncol=2, facecolor="plum", numpoints=1
            ,bbox_to_anchor=[-0.19, -0.19])
-plt.imshow(density) 
+plt.imshow(density_map) 
 plt.show() 
 
 
